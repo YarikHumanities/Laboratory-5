a2b141a962fabfa69c8b610ba7e65d06c5cf7fe0
Yarik <yaroslav.matsapura@gmail.com>
2021-05-14 11:39:53 +0300

    Distance function

--- Laboratory-5/Laboratory-5/Source.cpp
+++ Laboratory-5/Laboratory-5/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
+#define rad = 6367444.65;
 class Location {
 public:
 	double latitude;
@@ -21,4 +23,12 @@ public:
 	Rectangle(Location leftDown, Location leftUp, Location rightDown, Location rightUp) : 
 		up_left(leftUp), dowm_right(rightDown), up_right(rightUp), down_left(leftDown) {}
 
+	double lenght(double longitude1, double latitude1, double longitude2, double latitude2) {
+		double x1= (longitude1*2*acos(0.0))/180;
+		double y1= (latitude1*2*acos(0.0))/180;
+		double x2= (longitude2*2*acos(0.0))/180;
+		double y2= (latitude2*2*acos(0.0))/180;
+		
+		double result = 2 * asin(sqrt(pow(sin((x2 - x1) / 2), 2) + cos(x2) * cos(x1) * pow(sin((y2 - y1) / 2), 2)));
+	}
 };
