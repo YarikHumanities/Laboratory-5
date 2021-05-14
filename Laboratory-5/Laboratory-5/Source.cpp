c3381e6a6728f8163958e261c12e8a38ae1c7674
Yarik <yaroslav.matsapura@gmail.com>
2021-05-14 11:03:19 +0300

    Rectangle class init

--- Laboratory-5/Laboratory-5/Source.cpp
+++ Laboratory-5/Laboratory-5/Source.cpp
@@ -12,3 +12,13 @@ public:
 	Location(double latitude, double longitude, string type, string subtype, string name, string address) :
 		latitude(latitude), longitude(longitude), type(type), subtype(subtype), name(name), address(address){}
 };
+class Rectangle {
+public:
+	Location up_left;
+	Location down_left;
+	Location up_right;
+	Location dowm_right;
+	Rectangle(Location leftDown, Location leftUp, Location rightDown, Location rightUp) : 
+		up_left(leftUp), dowm_right(rightDown), up_right(rightUp), down_left(leftDown) {}
+
+};
